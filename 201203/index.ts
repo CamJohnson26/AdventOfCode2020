9fe36859ad6ca8867c2e42e5e405bdaf4aa296c2
Cameron Johnson <cameron@speakflash.com>
2020-12-02 23:18:04 -0600

    Part 2

--- 201203/index.ts
+++ 201203/index.ts
@@ -12,26 +12,37 @@ const readData = () => {
   return input;
 }
 
-const input = readData();
-// console.log(input)
-let result = 0
-let x = 0
-let y = 0
-if (input[x][y] === '#') {
-  result += 1
-}
-// input.shift()
-console.log(input.length)
-for (const row of input) {
-  x += 1
-  y += 3
+const count_trees = (input, x_shift, y_shift) => {
 
-  y = y % row.length;
-
-
-  if (row[y] === '#') {
+  let result = 0
+  let x = 0
+  let y = 0
+  if (input[x][y] === '#') {
     result += 1
   }
-}
+  for (const row of input) {
+    x += x_shift
+    y += y_shift
 
-console.log(result)
+    y = y % row.length;
+    if (x > input.length - 1) {
+      break;
+    }
+    if (input[x][y] === '#') {
+      result += 1
+    }
+  }
+
+  return result
+} 
+
+const input = readData();
+const results = [
+  count_trees(input, 1, 1),
+  count_trees(input, 1, 3),
+  count_trees(input, 1, 5),
+  count_trees(input, 1, 7),
+  count_trees(input, 2, 1),
+]
+
+console.log(results.reduce((acc, cur) => acc*cur, 1))
