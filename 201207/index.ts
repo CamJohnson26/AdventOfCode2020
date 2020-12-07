5b9a0c91ed08be52ffcec031f1a759c965e666de
Cameron Johnson <cameron@speakflash.com>
2020-12-06 23:57:48 -0600

    Part 2

--- 201207/index.ts
+++ 201207/index.ts
@@ -20,6 +20,7 @@ for (const group of input) {
   const outer = (group[0] as string).replace('bags.', '').replace('bag.', '').replace('bags', '').replace('bag', '').trim()
   // console.log(outer)
   for (const element of group[1]) {
+    const num = parseInt(element.split(' ')[0])
     const parsed = element.slice(2, element.length).replace('bags.', '').replace('bag.', '').replace('bags', '').replace('bag', '').trim()
     if (parsed === ' other' || parsed === ' other ' || parsed === 'other ' || parsed.includes('other')) {
       continue;
@@ -27,52 +28,42 @@ for (const group of input) {
 
       // console.log(outer)
       if (Object.keys(graphMap).includes(outer)) {
-        graphMap[outer].add(parsed)
+        graphMap[outer].add({val: parsed, num})
       } else {
-        graphMap[outer] = new Set([parsed])
+        graphMap[outer] = new Set([{val: parsed, num}])
       }
       // console.log(parsed)
     }
   }
 }
-
+console.log(graphMap)
 const all = new Set(Object.keys(graphMap))
 const memo = {}
 
 function depthFirst(element) {
-  // console.log(element)
+  console.log('-----------------')
+  console.log(element)
   // console.log(memo)
-  element === 'dark orange' && console.log(element)
-  if (element === 'shiny gold') {
-    return false
-  }
-  if (Object.keys(memo).includes(element)) {
-    return memo[element]
+  if (Object.keys(memo).includes(element.val)) {
+    return memo[element.val]
   } else {
-    if (!Object.keys(graphMap).includes(element)) {
-      return false;
+    if (!Object.keys(graphMap).includes(element.val)) {
+      return 1;
     }
-    if (Array.from(graphMap[element]).includes('shiny gold')) {
-      // console.log(true)
-      return true;
-    }
-    // console.log(Array.from(graphMap[element]))
-    return Array.from(graphMap[element]).reduce((acc, cur) => {
-      return acc || depthFirst(cur)
-    }, false);
+    console.log(Array.from(graphMap[element.val]))
+    return (Array.from(graphMap[element.val]).reduce((acc: number, cur: {val: string, num: number}) => {
+      // console.log(acc, cur.val, cur.num, depthFirst(cur),acc + (cur.val === 'shiny gold' ? 0 : (depthFirst(cur)))* cur.num)
+      const rv = acc + (cur.val === 'shiny gold' ? 0 : (depthFirst(cur)))* cur.num
+      console.log(element, rv)
+      return rv
+    }, 0)) as number + 1;
   }
 }
 
-console.log(graphMap)
 
 let count = 0;
-for (const a of Array.from(all)) {
-  if (depthFirst(a)) {
-    count += 1
-    console.log(a)
-  }
-}
-
+  count += depthFirst({val: 'shiny gold', num: 1})
+count -= 1
 console.log(count)
 // console.log(all)
 
