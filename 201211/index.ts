c920376c8a6bacd23a5ef3e397a601118093de6b
Cameron Johnson <cameron@speakflash.com>
2020-12-10 23:39:03 -0600

    Part 2

--- 201211/index.ts
+++ 201211/index.ts
@@ -43,25 +43,42 @@ const getSeatState = (seat, adj: string[]) => {
     }
   }
   if (seat === '#') {
-    if (adj.filter(a => a === '#').length >= 4) {
+    if (adj.filter(a => a === '#').length >= 5) {
       return 'L'
     }
   }
   return seat
 }
 
+const getAdjacent = (i, j, state) => {
+  let ni = -1;
+  let nj = -1;
+  const adj = []
+  while (ni <= 1) {
+    while (nj <= 1) {
+      if (!(ni === 0 && nj === 0)) {
+        let di = i + ni
+        let dj = j + nj
+        while ((state[di]||{})[dj] === '.') {
+          di = di + ni;
+          dj = dj + nj;
+        }
+        if ((state[di]||{})[dj] !== undefined) {
+          adj.push(state[di][dj])
+        }
+  
+      }
+      nj += 1;
+    }
+    nj = -1
+    ni += 1;
+  }
+  return adj
+}
+
 const nextState = (state: string[][]) => {
 
-  return state.map((row, i) => row.map((seat, j) => getSeatState(seat, [
-    (state[i-1]||{})[j-1],
-    (state[i-1]||{})[j],
-    (state[i-1]||{})[j+1],
-    (state[i]||{})[j-1],
-    (state[i]||{})[j+1],
-    (state[i+1]||{})[j-1],
-    (state[i+1]||{})[j],
-    (state[i+1]||{})[j+1],
-  ])))
+  return state.map((row, i) => row.map((seat, j) => getSeatState(seat, getAdjacent(i, j, state))))
 }
 
 const statesMatch = (s1, s2) => {
@@ -82,9 +99,11 @@ const statesMatch = (s1, s2) => {
 
 let state = input
 let oldState = []
+let counter = 0
 while (!statesMatch(state, oldState)) {
   oldState = state;
   state = nextState(state)
+  counter += 1
 }
 
 let count = 0
