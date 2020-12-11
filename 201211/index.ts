c1663a1e35f4d3c23acd4f41256daf5b43320fe5
Cameron Johnson <cameron@speakflash.com>
2020-12-10 23:16:18 -0600

    Part 1

--- 201211/index.ts
+++ 201211/index.ts
@@ -24,16 +24,71 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const groupSplit = "\n"
-const elementSplit = " "
+// const elementSplit = " "
 const readData = () => {
   const file_path = 'input.txt'
   const data = fs.readFileSync(path.resolve(__dirname, file_path))
   const input = data.toString().trim()
     .split(groupSplit)
-    .map(d => d.split(elementSplit))
+    .map(d => d.split(''))
   return input;
 }
 
 const input = readData();
 
-console.log(input)
+const getSeatState = (seat, adj: string[]) => {
+  if (seat === 'L') {
+    if (adj.filter(a => a === '#').length === 0) {
+      return '#'
+    }
+  }
+  if (seat === '#') {
+    if (adj.filter(a => a === '#').length >= 4) {
+      return 'L'
+    }
+  }
+  return seat
+}
+
+const nextState = (state: string[][]) => {
+
+  return state.map((row, i) => row.map((seat, j) => getSeatState(seat, [
+    (state[i-1]||{})[j-1],
+    (state[i-1]||{})[j],
+    (state[i-1]||{})[j+1],
+    (state[i]||{})[j-1],
+    (state[i]||{})[j+1],
+    (state[i+1]||{})[j-1],
+    (state[i+1]||{})[j],
+    (state[i+1]||{})[j+1],
+  ])))
+}
+
+const statesMatch = (s1, s2) => {
+  if (s1.length !== s2.length) {
+    return false
+  }
+  if (s1[0].length !== s2[0].length) {
+    return false
+  }
+
+  for (let i = 0; i < s1.length; i++) {
+    for (let j = 0; j < s1.length; j++) {
+      if (s1[i][j] != s2[i][j]) return false
+    }
+  }
+  return true
+}
+
+let state = input
+let oldState = []
+while (!statesMatch(state, oldState)) {
+  oldState = state;
+  state = nextState(state)
+}
+
+let count = 0
+console.log(state)
+state.forEach(row => row.forEach(seat => seat === '#' ? count += 1 : null))
+console.log(count)
+// console.log(nextState(input))
